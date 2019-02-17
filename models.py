07f782843be32fc648857f22a2a386e9f64b8415
rusti <mrgregory1@gmail.com>
2019-02-16 22:02:30 -0500

    hmmm

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = "person"
+    __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(30))
