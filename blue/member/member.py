d73426b1ffcdbf00f5af59018bae749e1729acbc
Doug Farrell <doug.farrell@gmail.com>
2019-02-18 12:52:43 -0500

    I've added a Python module called config.py to the main directory. This is where all of the configuration for the application. This includes creating the Flask app, the db connection, and other stuff. In this way other modules can import the various parts they need, like models.py only needs the db instance. See what I've done with the other modules. The application runs and the login and member_page renders. I'm not all that familiar with the Flask session object, so I'm not sure what goes on there. The session seems to persist even after the server is stopped. Maybe you can help me understand the session object.

--- blue/member/member.py
+++ blue/member/member.py
@@ -1,32 +1,51 @@
 from flask import (
-    Flask,
     Blueprint,
-    session,
-    render_template,
-    url_for,
-    redirect,
-    request,
-    flash,
 )
 
-import pathlib
-from functions import *
-from flask_login import LoginManager, UserMixin, login_user, login_required
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, login_required
+
+# we need user from models, so we grab it here
 from models import User
+
+# all we need is login_manager, so grab it from comnfig here
+from config import login_manager
 
 
 member = Blueprint("member", __name__, template_folder="member")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # I like this style just because it's more explicit
+    return User.query.filter(User.id == int(user_id)).first()
 
 
 @member.route("/login")
 def member_login():
     user = User.query.filter_by(username="rusti").first()
     login_user(user)
-    return "<h1>you are logged in</h1>"
+    return """
+    <html>
+    <head>
+        <title>Logged in page</title>
+    </head>
+    <body>
+        <h1>you are logged in</h1>
+    </body>
+    </html>
+    """
 
 
 @member.route("/member_page")
 @login_required
 def member_page():
-    return "</h1>you are in a protected page</h1>"
+    return """
+    <html>
+    <head>
+        <title>Member Page</title>
+    </head>
+    <body>
+        <h1>you are in a protected page</h1>
+    </body>
+    </html>
+    """
--- myapp.py
+++ myapp.py
@@ -1,66 +1,19 @@
 from flask import (
-    Flask,
-    session,
     render_template,
     url_for,
     redirect,
-    request,
-    flash,
-    Blueprint,
 )
-import random
-import json
-import pathlib
 from functions import *
 from blue.breakaway.breakaway import breakaway
 from blue.member.member import member
-import pprint
-import os
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required
+
+# we need the Flask app instance, so we get it from config
+from config import app
 
 
-app = Flask(__name__)
-
+# register the blueprints using the app instance created in the config module
 app.register_blueprint(breakaway, url_prefix="/breakaway")
 app.register_blueprint(member, url_prefix="/member")
-
-# set a 'SECRET_KEY' to enable the Flask session cookies
-app.config["SECRET_KEY"] = "not_very_secret"
-
-basedir = pathlib.Path(__file__).parent.resolve()
-basedir_os = os.path.abspath(os.path.dirname(__file__))
-
-login_manager = LoginManager(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-    # return User.get(user_id)
-
-
-# Build the Sqlite ULR for SqlAlchemy
-sqlite_url = "sqlite:////" + os.path.join(basedir_os, "flamme_rouge.db")
-
-# Configure the SqlAlchemy part of the app instance
-app.config["SQLALCHEMY_ECHO"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# Create the SqlAlchemy db instance
-db = SQLAlchemy(app)
-
-
-from flask_debugtoolbar import DebugToolbarExtension
-
-# the toolbar is only enabled in debug mode:
-app.debug = True
-toolbar = DebugToolbarExtension(app)
-# DEBUG_TB_INTERCEPT_REDIRECTS = False
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-# Set the secret key to the debugtoolbar
-app.secret_key = "my_secret"
 
 
 @app.route("/")
